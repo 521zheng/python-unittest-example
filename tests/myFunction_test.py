1a7ba2defd6e01124d146a251182f261ada40964
ksalihcavusoglu <ksalihcavusoglu@users.noreply.github.com>
2019-12-30 22:44:04 +0300

    Update myFunction_test.py

--- tests/myFunction_test.py
+++ tests/myFunction_test.py
@@ -4,4 +4,4 @@ from .context import src
 class SumTest(unittest.TestCase):
     def test_Sum(self):
         self.assertEqual(src.Sum(1, 1), 2)
-        self.assertTrue(src.Sum(1, 2) == 3)
+        self.assertTrue(src.Sum(1, 2) == 4)
